e38c94465bfe748e87bcd2ac4b24958253d71310
GeorgeGUS <georgegus1@gmail.com>
2019-05-23 15:33:25 +0300

    Updates

---
+++ precache-manifest.c936816f36f9b2e2c2f7dcc8487f70f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe84a4502c744dc9024a4cb3811d1d67",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "2ca4bde07fa79dbaf513",
+    "url": "/kanban/static/css/main.db629dc2.chunk.css"
+  },
+  {
+    "revision": "176cfd98696e5a7590ff",
+    "url": "/kanban/static/js/2.23200386.chunk.js"
+  },
+  {
+    "revision": "2ca4bde07fa79dbaf513",
+    "url": "/kanban/static/js/main.0fe45a93.chunk.js"
+  },
+  {
+    "revision": "9c81d15565ccb12a85c0",
+    "url": "/kanban/static/js/runtime~main.4f25e9d8.js"
+  },
+  {
+    "revision": "73a23ecb852491ac5ed93c9b2ba79eba",
+    "url": "/kanban/static/media/app-bg.73a23ecb.jpg"
+  },
+  {
+    "revision": "18202fbef1e52e00bd1050af7d38a2ec",
+    "url": "/kanban/static/media/cross.18202fbe.svg"
+  },
+  {
+    "revision": "25d2378b1f59d5d4708b12d48e6cab26",
+    "url": "/kanban/static/media/plus.25d2378b.svg"
+  }
+]);
